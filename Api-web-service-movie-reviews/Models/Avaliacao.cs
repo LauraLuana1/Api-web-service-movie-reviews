2ffd1c290150a707f132d62d1da7854b8a4b8346
Laura Luana <laura.luana@sga.pucminas.br>
2025-03-17 12:05:54 -0300

    Update Models: Filme e Avaliacao

---
+++ Api-web-service-movie-reviews/Models/Avaliacao.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api_web_service_movie_reviews.Models
+{
+    [Table("Avaliações")]
+    public class Avaliacao
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Resenha { get; set; }
+        [Required]
+        public DateTime DataAvaliacao { get; set; }
+        [Required]
+        public int FilmeId { get; set; }
+        public Filme Filme { get; set; }
+    }
+}
---
+++ Api-web-service-movie-reviews/Models/Filme.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api_web_service_movie_reviews.Models
+{
+    [Table("Filmes")]
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Genero { get; set; }
+        [Required]
+        public string Sinopse { get; set; }
+        [Required]
+        public int AnoLancamento { get; set; }
+        [Required]
+        public decimal Nota { get; set; }
+        public ICollection<Avaliacao> Avaliações { get; set; }
+
+    }
+}
